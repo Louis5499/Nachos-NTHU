9553ed960acfd4eaa82eb223414edbcd7eb11da7
Mencher Chiang <louis@9dynamics.com.tw>
2019-10-10 16:41:44 +0800

    first version

---
+++ code/userprog/ksyscall.h
@@ -0,0 +1,60 @@
+/**************************************************************
+ *
+ * userprog/ksyscall.h
+ *
+ * Kernel interface for systemcalls 
+ *
+ * by Marcus Voelp  (c) Universitaet Karlsruhe
+ *
+ **************************************************************/
+
+#ifndef __USERPROG_KSYSCALL_H__ 
+#define __USERPROG_KSYSCALL_H__ 
+
+#include "kernel.h"
+
+#include "synchconsole.h"
+
+
+void SysHalt()
+{
+  kernel->interrupt->Halt();
+}
+
+void SysPrintInt(int val)
+{ 
+  DEBUG(dbgTraCode, "In ksyscall.h:SysPrintInt, into synchConsoleOut->PutInt, " << kernel->stats->totalTicks);
+  kernel->synchConsoleOut->PutInt(val);
+  DEBUG(dbgTraCode, "In ksyscall.h:SysPrintInt, return from synchConsoleOut->PutInt, " << kernel->stats->totalTicks);
+}
+
+int SysAdd(int op1, int op2)
+{
+  return op1 + op2;
+}
+
+int SysCreate(char *filename)
+{
+	// return value
+	// 1: success
+	// 0: failed
+	return kernel->fileSystem->Create(filename);
+}
+
+int SysOpen(char *filename) {
+  return kernel->fileSystem->OpenAFile(filename);
+}
+
+int SysWrite(char *buffer, int size, int id) {
+  return kernel->fileSystem->Write(buffer, size, id);
+}
+
+int SysRead(char *buffer, int size, int id) {
+  return kernel->fileSystem->Read(buffer, size, id);
+}
+
+int SysClose(int id) {
+  return kernel->fileSystem->Close(id);
+}
+
+#endif /* ! __USERPROG_KSYSCALL_H__ */
